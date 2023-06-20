e6a5aff8f78c4cc749f3a1ac54a455a45ecaaa3d
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-06-20 15:29:10 -0400

    test

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -38,64 +38,64 @@ export async function GET() {
   }
 }
 
-// id SERIAL PRIMARY KEY,
-//     paper_type VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-//     paper_weight VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-//     paper_description VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-//     multiplier FLOAT,
-//     category ENUM('Fine Art', 'Photo') DEFAULT 'Fine Art'
+// // id SERIAL PRIMARY KEY,
+// //     paper_type VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+// //     paper_weight VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+// //     paper_description VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+// //     multiplier FLOAT,
+// //     category ENUM('Fine Art', 'Photo') DEFAULT 'Fine Art'
 
-export async function PUT({ body }) {
-  try {
-    const results = await queryDatabase(
-      "SELECT * FROM `papers` WHERE `id` = '${body.id}"
-    );
-    if (results.length === 0) {
-      return new NextResponse(404, "Paper not found.");
-    }
-    const paper = results[0];
-    const updatedPaper = { ...paper, ...body };
-    await queryDatabase(
-      `UPDATE \`papers\` SET \`paper_type\` = ${body.paper_type}, \`paper_weight\` = ${body.paper_weight}, \`paper_description\` = ${body.paper_description}, \`multiplier\` = ${body.multiplier}, \`category\` = ${body.category} WHERE \`id\` = ${body.id};`
-    );
-    connection.end();
-    return NextResponse.json(results);
-  } catch (error) {
-    connection.end();
-    console.error("Error executing query:", error);
-    return new NextResponse(
-      500,
-      "An error occurred when querying the database."
-    );
-  }
-}
+// export async function PUT({ body }) {
+//   try {
+//     const results = await queryDatabase(
+//       "SELECT * FROM `papers` WHERE `id` = '${body.id}"
+//     );
+//     if (results.length === 0) {
+//       return new NextResponse(404, "Paper not found.");
+//     }
+//     const paper = results[0];
+//     const updatedPaper = { ...paper, ...body };
+//     await queryDatabase(
+//       `UPDATE \`papers\` SET \`paper_type\` = ${body.paper_type}, \`paper_weight\` = ${body.paper_weight}, \`paper_description\` = ${body.paper_description}, \`multiplier\` = ${body.multiplier}, \`category\` = ${body.category} WHERE \`id\` = ${body.id};`
+//     );
+//     connection.end();
+//     return NextResponse.json(results);
+//   } catch (error) {
+//     connection.end();
+//     console.error("Error executing query:", error);
+//     return new NextResponse(
+//       500,
+//       "An error occurred when querying the database."
+//     );
+//   }
+// }
 
-export async function POST() {
-  try {
-    const results = await queryDatabase("SELECT * FROM `papers`");
-    connection.end();
-    return NextResponse.json(results);
-  } catch (error) {
-    connection.end();
-    console.error("Error executing query:", error);
-    return new NextResponse(
-      500,
-      "An error occurred when querying the database."
-    );
-  }
-}
+// export async function POST() {
+//   try {
+//     const results = await queryDatabase("SELECT * FROM `papers`");
+//     connection.end();
+//     return NextResponse.json(results);
+//   } catch (error) {
+//     connection.end();
+//     console.error("Error executing query:", error);
+//     return new NextResponse(
+//       500,
+//       "An error occurred when querying the database."
+//     );
+//   }
+// }
 
-export async function DELETE() {
-  try {
-    const results = await queryDatabase("SELECT * FROM `papers`");
-    connection.end();
-    return NextResponse.json(results);
-  } catch (error) {
-    connection.end();
-    console.error("Error executing query:", error);
-    return new NextResponse(
-      500,
-      "An error occurred when querying the database."
-    );
-  }
-}
+// export async function DELETE() {
+//   try {
+//     const results = await queryDatabase("SELECT * FROM `papers`");
+//     connection.end();
+//     return NextResponse.json(results);
+//   } catch (error) {
+//     connection.end();
+//     console.error("Error executing query:", error);
+//     return new NextResponse(
+//       500,
+//       "An error occurred when querying the database."
+//     );
+//   }
+// }
