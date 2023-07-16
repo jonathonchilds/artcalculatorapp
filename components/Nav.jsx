75e498a6f4b6499a8bb77fa16ddfc5e80160c318
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-07-16 16:09:24 -0400

    styling fixes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="sm:flex sm:flex-row flex-col justify-center text-center space-y-6 sm:space-y-0 py-6 bg-[#191717] w-full px-12 sm:sticky sm:top-0 sm:justify-evenly items-center">
+    <nav className="sm:flex sm:flex-row flex-col justify-center text-center space-y-6 sm:space-y-0 py-10 bg-[#191717] w-full px-12 sm:sticky sm:top-0 sm:justify-evenly items-center">
       <div className="sm:flex hidden text-white w-[400px] text-3xl font-light tracking-[10px]">
         (866) 352-9779
       </div>
